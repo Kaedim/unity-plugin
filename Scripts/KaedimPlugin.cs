480bc1134623ca4071ecca30a95bc2ea19bc3be3
marinouj <marinoujoanna@gmail.com>
2023-06-22 22:24:36 +0100

    first commit

---
+++ Scripts/KaedimPlugin.cs
@@ -0,0 +1,358 @@
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+using UnityEngine.Networking;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using Unity.VisualScripting;
+
+[System.Serializable]
+public class AssetList
+{
+    public List<Asset> assets;
+}
+
+[System.Serializable]
+public class Asset
+{
+    public string requestID;
+    public List<string> image;
+    public List<string> image_tags;
+    public List<Iteration> iterations;
+    public Texture2D texture;
+}
+
+[System.Serializable]
+public class Iteration
+{
+    public string iterationID;
+    public Results results;
+    public string status;
+}
+
+[System.Serializable]
+public class Results
+{
+    public string fbx;
+    public string glb;
+    public string mtl;
+    public string obj;
+    public string gltf;
+}
+
+[System.Serializable]
+public class ResponseData
+{
+    public string jwt;
+    public string message;
+}
+public class KaedimPlugin : EditorWindow
+{
+
+    private class EditorCoroutine
+    {
+        public IEnumerator Routine { get; private set; }
+        public bool IsDone { get; private set; }  // Define the IsDone property here
+
+        public EditorCoroutine(IEnumerator routine)
+        {
+            this.Routine = routine;
+            this.IsDone = false; // Set the initial value for IsDone
+        }
+
+        public void MarkAsDone()
+        {
+            this.IsDone = true;
+        }
+    }
+
+    private List<EditorCoroutine> _coroutines = new List<EditorCoroutine>();
+    string devID = "";
+    string apiKey = "";
+    string jwt = "";
+    string state = "";
+    string refreshToken = "";
+    static string domain = "http://localhost:4200";
+    List<Asset> assets = new List<Asset>();
+    Asset selectedAsset = null;
+
+    // Add menu named "KaedimPlugin" to the Window menu
+    [MenuItem("Window/Kaedim Plugin")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        KaedimPlugin window = (KaedimPlugin)EditorWindow.GetWindow(typeof(KaedimPlugin));
+        window.Show();
+    }
+
+    void OnEnable()
+    {
+        // Load saved IDs when the window is enabled
+        state = "login";
+        devID = EditorPrefs.GetString("DevID", "");
+        apiKey = EditorPrefs.GetString("APIKey", "");
+        refreshToken = EditorPrefs.GetString("RefreshToken", "");
+        jwt = EditorPrefs.GetString("jwt", "");
+    }
+
+    private Vector2 scrollPosition = Vector2.zero;
+
+    void OnGUI()
+    {
+        if (state == "login")
+        {
+            GUILayout.Label("Enter your credentials", EditorStyles.boldLabel);
+            devID = EditorGUILayout.TextField("Dev ID", devID);
+            apiKey = EditorGUILayout.TextField("API Key", apiKey);
+            refreshToken = EditorGUILayout.TextField("Refresh Token", refreshToken);
+            if (GUILayout.Button("Login"))
+            {
+                PerformLoginRequest();
+            }
+        }
+        if (state == "load_assets")
+        {
+            GUILayout.Label("Successful Login", EditorStyles.boldLabel);
+            if (GUILayout.Button("Load assets"))
+            {
+                PerformFetchAssetRequest();
+            }
+        }
+        if (state == "asset_library")
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Logout"))
+            {
+                Debug.Log("Log out");
+            }
+            if (GUILayout.Button("Refresh assets"))
+            {
+                Debug.Log($"Refreshing assets");
+                PerformFetchAssetRequest();
+            }
+            EditorGUILayout.EndHorizontal();
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            foreach (Asset asset in assets)
+            {
+                string name = "Asset";
+                if (asset.image_tags.Count > 0)
+                    name = asset.image_tags[0];
+                if (GUILayout.Button(new GUIContent(name)))
+                {
+                    SelectAsset(asset);
+                }
+            }
+            EditorGUILayout.EndScrollView();
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Import asset", GUILayout.Width(150)))
+            {
+                StartEditorCoroutine(DownloadOBJ());
+                Debug.Log($"Import asset");
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        
+    }
+
+
+    private void StartEditorCoroutine(IEnumerator routine)
+    {
+        var coroutine = new EditorCoroutine(routine);
+        _coroutines.Add(coroutine);
+        EditorApplication.update += UpdateEditorCoroutine;
+    }
+
+    private void UpdateEditorCoroutine()
+    {
+        for (int i = 0; i < _coroutines.Count; i++)
+        {
+            if (!_coroutines[i].Routine.MoveNext())
+            {
+                _coroutines[i].MarkAsDone();
+            }
+        }
+
+        _coroutines.RemoveAll(c => c.IsDone);
+
+        if (_coroutines.Count == 0)
+        {
+            EditorApplication.update -= UpdateEditorCoroutine;
+        }
+    }
+
+    void PerformLoginRequest()
+    {
+        EditorApplication.update -= PerformLoginRequest; // Remove this function from the update delegate since it only needs to run once
+
+        string url = domain + "/api/v1/registerHook";
+        string json = "{ \"devID\": \"" + devID + "\", \"destination\": \"https://unity.kaedim3d.com/webhook\" }";
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("X-API-Key", apiKey);
+
+        // Send the request
+        var operation = request.SendWebRequest();
+
+        // Wait for the request to complete (synchronously)
+        while (!operation.isDone)
+        {
+            Thread.Sleep(100);
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            EditorPrefs.SetString("DevID", devID);
+            EditorPrefs.SetString("APIKey", apiKey);
+            PerformJwtRequest();
+        }
+        else
+        {
+            Debug.Log("Request error: " + request.error);
+        }
+    }
+
+    void PerformJwtRequest()
+    {
+        EditorApplication.update -= PerformJwtRequest; // Remove this function from the update delegate since it only needs to run once
+
+        string url = domain + "/api/v1/refreshJWT";
+        string json = "{ \"devID\": \"" + devID + "\"}";
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("X-API-Key", apiKey);
+        request.SetRequestHeader("refresh-token", refreshToken);
+
+        // Send the request
+        var operation = request.SendWebRequest();
+
+        // Wait for the request to complete (synchronously)
+        while (!operation.isDone)
+        {
+            Thread.Sleep(100);
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            string response = request.downloadHandler.text; // Get the JSON string from the response
+            ResponseData data = JsonUtility.FromJson<ResponseData>(response); // Deserialize it
+            jwt = data.jwt;
+            EditorPrefs.SetString("RefreshToken", refreshToken);
+            EditorPrefs.SetString("jwt", jwt);
+            state = "load_assets";
+        }
+        else
+        {
+            Debug.Log("Request error: " + request.downloadHandler.text);
+        }
+    }
+
+
+    void PerformFetchAssetRequest()
+    {
+        EditorApplication.update -= PerformFetchAssetRequest; // Remove this function from the update delegate since it only needs to run once
+
+        string url = domain + "/api/v1/fetchAll";
+        string json = "{ \"devID\": \"" + devID + "\"}";
+        var request = new UnityWebRequest(url, "GET");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("X-API-Key", apiKey);
+        request.SetRequestHeader("Authorization", jwt);
+
+        // Send the request
+        var operation = request.SendWebRequest();
+
+        // Wait for the request to complete (synchronously)
+        while (!operation.isDone)
+        {
+            Thread.Sleep(100);
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Request completed. Output: " + request.downloadHandler.text);
+            var data = JsonUtility.FromJson<AssetList>(request.downloadHandler.text);
+            state = "asset_library";
+            assets = data.assets;
+            Debug.Log("ParsedData: " + data.assets);
+            foreach (Asset asset in data.assets)
+            {
+                if (asset.image.Count <= 0)
+                    continue;
+                Debug.Log("Asset: " + asset.image[0]);
+                StartEditorCoroutine(DownloadImage(asset));
+            }
+
+        }
+        else
+        {
+            Debug.Log("Request error: " + request.downloadHandler.text);
+        }
+    }
+
+
+    void SelectAsset(Asset asset)
+    {
+        selectedAsset = asset;
+    }
+    IEnumerator DownloadImage(Asset data)
+    {
+        Debug.Log(data);
+
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(data.image[0]);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            data.texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        }
+    }
+
+    IEnumerator DownloadOBJ()
+    {
+        int len = selectedAsset.iterations.Count;
+        string url = selectedAsset.iterations[len - 1].results.obj;
+        Debug.Log("Url" + selectedAsset.requestID + len +  url);
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            yield return www.SendWebRequest();
+            while (www.result == UnityWebRequest.Result.InProgress)
+            {
+                Debug.Log("Res" + www.result);
+                Thread.Sleep(100);
+            }
+            Debug.Log("Res" + www.result);
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+
+                File.WriteAllBytes(Application.dataPath + "/downloaded.obj", results);
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+
+}
